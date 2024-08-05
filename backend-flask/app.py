851efcb82581f14058b9ed64dc6110f63a6d8839
jadev <julianamindev@gmail.com>
2024-08-05 09:05:13 +0800

    added user logic for test

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,23 +21,15 @@ logging.basicConfig(
 def home():
     app.logger.debug("flask started")
 
-    # headers = request.headers
-
-    # app.logger.debug(f"headers: {headers}")
-    
-    # user = request.headers.get("X-Auth-User")
     app.logger.debug(f"environ: {request.environ}")
     
-    # app.logger.debug(f"user: {user}")
-    
-    # str = f"hi there {user}"
-    return f"hi {request.environ.get("HTTP_REMOTE_USER")}"
-    # user = request.environ.get("REMOTE_USER")
-    # # app.logger.debug(f"User access: {user}")
-    # if user:
-    #     return f"Logged in as {user}"
-    # else:
-    #     return "No user logged in"
+    user = request.environ.get("HTTP_REMOTE_USER")
+
+    if not user:
+        user = "jamin"
+
+    return f"hi {user}"
+
     
 if __name__ == "__main__":
     app.run()
